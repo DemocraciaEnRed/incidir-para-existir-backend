2ee29e221e595acd27833c265e39698b793dbb2b
Guillermo Croppi <guillermocroppi@gmail.com>
2025-01-28 13:41:10 -0300

    Add testController and routes for dimensions query, account verification, initiative and challenge creation, and blog entry generation

---
+++ controllers/testController.js
@@ -0,0 +1,205 @@
+
+const { faker } = require('@faker-js/faker');
+const models = require('../models');
+const dayjs = require('dayjs');
+const msg = require('../utils/messages');
+const UtilsHelper = require('../helpers/utilsHelper');
+
+exports.dimensionsQuery = async (req, res) => {
+  try {
+    console.log(req.query.id)
+    return res.status(200).json(req.query.id)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({message: error.message})
+  }
+}
+
+exports.verifyAccount = async (req, res) => {
+  try {
+    const html = await mailer.renderHtml('auth/successVerification', {
+      appUrl: process.env.APP_URL
+    })
+    return res.status(200).send(html)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({message: error.message})
+  }
+}
+
+exports.createInitiatives = async (req, res) => {
+  try {
+
+    const dimensions = await models.Dimension.findAll()
+
+    // make a loop of 150 iterations
+    for(let i = 0; i < 150; i++) {
+
+
+      const contact = {
+        fullname: faker.person.fullName(),  
+        email: faker.internet.email(),
+        keepEmailPrivate: faker.datatype.boolean(),
+        phone: faker.phone.number(),
+        keepPhonePrivate: faker.datatype.boolean()
+      }
+
+
+
+      const initiative = {
+        name: faker.lorem.sentence({min: 4, max: 15}),
+        source: 'web',
+        description: faker.lorem.sentence({min: 10, max: 50}),
+        needsAndOffers: faker.lorem.sentence({min: 20, max: 90}),
+        publishedAt: new Date(),
+        latitude: null,
+        longitude: null
+      }
+      const subdivisions = await models.Subdivision.findAll({include: 'city'})
+      // console.log(JSON.stringify(subdivisions,null,2))
+
+      // get a random subdivision
+      const randomSubdivision = subdivisions[Math.floor(Math.random() * subdivisions.length)]
+
+      const setCoordinates = faker.datatype.boolean(0.75)
+      const [fakeLatitude, fakeLongitude] = faker.location.nearbyGPSCoordinate({
+        origin: [randomSubdivision.latitude, randomSubdivision.longitude],
+        radius: 14,
+        isMetric: true
+      })
+      if(setCoordinates) {
+        initiative.latitude = fakeLatitude
+        initiative.longitude = fakeLongitude
+      }
+
+      const newContact = await models.InitiativeContact.create(contact)
+      const newInitiative = await models.Initiative.create({
+        ...initiative,
+        contactId: newContact.id,
+        subdivisionId: randomSubdivision.id,
+      })
+
+      // pick two random dimensions, different from each other
+      const randomIndex1 = Math.floor(Math.random() * dimensions.length)
+      let randomIndex2 = Math.floor(Math.random() * dimensions.length)
+      while(randomIndex2 === randomIndex1) {
+        randomIndex2 = Math.floor(Math.random() * dimensions.length)
+      }
+      const dimension1 = dimensions[randomIndex1]
+      const dimension2 = dimensions[randomIndex2]
+      
+      // make a 65% chance of adding both dimensions, the rest, add the first one
+      const chance = Math.random()
+      if(chance > 0.35) {
+        await newInitiative.addDimensions([dimension1, dimension2])
+      } else {
+        await newInitiative.addDimensions([dimension1])
+      }
+    }
+
+    return res.status(200).json({
+      message: '150 Initiatives created successfully'
+    })
+
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({message: error.message})
+  }
+}
+
+exports.createChallenges = async (req, res) => {
+  try {
+    // challenge has:
+    //    dimensionId
+    //    subdivisionId
+    //    needsAndChallenges
+    //    proposal
+    //    inWords
+    //    latitude: null
+    //    longitude: null
+    
+    const dimensions = await models.Dimension.findAll()
+    const subdivisions = await models.Subdivision.findAll()
+
+    // make a loop of 150 iterations
+    for(let i = 0; i < 150; i++) {
+      const challenge = {
+        needsAndChallenges: faker.lorem.sentence({min: 20, max: 90}),
+        proposal: faker.lorem.sentence({min: 20, max: 90}),
+        inWords: faker.lorem.words({min: 1, max: 2}),
+        latitude: null,
+        longitude: null
+      }
+
+      // get a random subdivision
+      const randomSubdivision = subdivisions[Math.floor(Math.random() * subdivisions.length)]
+      const randomDimension = dimensions[Math.floor(Math.random() * dimensions.length)]
+
+      const setCoordinates = faker.datatype.boolean(0.75)
+      const [fakeLatitude, fakeLongitude] = faker.location.nearbyGPSCoordinate({
+        origin: [randomSubdivision.latitude, randomSubdivision.longitude],
+        radius: 14,
+        isMetric: true
+      })
+      if(setCoordinates) {
+        challenge.latitude = fakeLatitude
+        challenge.longitude = fakeLongitude
+      }
+
+      const newChallenge = await models.Challenge.create({
+        subdivisionId: randomSubdivision.id,
+        dimensionId: randomDimension.id,
+        ...challenge,
+      })
+    }
+
+    return res.status(200).json({
+      message: '150 Challenges created successfully'
+    })
+  } catch {
+    console.error(error)
+    return res.status(500).json({message: error.message})
+  }
+}
+
+exports.fileTest = async (req, res) => {
+  try {
+    console.log(req.file)
+    return res.status(200).json({message: 'File uploaded successfully', file: req.file })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({message: error.message})
+  }
+}
+
+
+exports.createBlogEntries = async (req, res) => {
+  try {
+    const categories = await models.BlogCategory.findAll();
+    const sections = await models.BlogSection.findAll();
+    const authors = await models.User.findAll();
+
+    for(let i = 0; i < 50; i++) {
+      const category = categories[Math.floor(Math.random() * categories.length)];
+      const section = sections[Math.floor(Math.random() * sections.length)];
+      const author = authors[Math.floor(Math.random() * authors.length)];
+
+      await models.BlogEntry.create({
+        title: faker.lorem.words({min: 8, max: 16}),
+        subtitle: faker.lorem.sentence(16),
+        text: faker.lorem.paragraphs({min: 12, max: 24}),
+        imageUrl: faker.image.urlPicsumPhotos({ width: 1024, height: 768, grayscale: false, blur: 0 }),
+        slug: faker.lorem.slug(5),
+        authorId: author.id,
+        categoryId: category.id,
+        sectionId: section.id,
+        publishedAt: dayjs().subtract(Math.floor(Math.random() * 365), 'day').toDate(),
+      });
+    }
+
+    return res.status(200).json({ message: 'Blog posts created' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: msg.error.default });
+  }
+}
--- routes/test.js
+++ routes/test.js
@@ -7,168 +7,29 @@ const { faker } = require('@faker-js/faker');
 const uploader = require('../middlewares/s3');
 const { query, check } = require('express-validator');
 const validate = require('../middlewares/validate');
-
-router.get('/', (req, res) => {
-  try {
-    return res.status(200).json({message: 'Test route works!'})
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({message: error.message})
-  }
-})
+const TestController = require('../controllers/testController');
 
 // ids is a string of comma separated integers
-router.get('/dimensions', 
+router.get('/dimensions-query', 
   [
     query('id.*').isInt().withMessage('id must be an array of integers'),
   ],
   validate,
-  async (req, res) => {
-    try {
-      console.log(req.query.id)
-      return res.status(200).json(req.query.id)
-    } catch (error) {
-      console.error(error)
-      return res.status(500).json({message: error.message})
-    }
-  }
+  TestController.dimensionsQuery
 )
 
-router.get('/cities', async (req, res) => {
-  try {
-    const cities = await models.City.findAll()
-    return res.status(200).json(cities)
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({message: error.message})
-  }
-})
+router.get('/verify-account', TestController.verifyAccount)
 
-router.get('/verifyaccount', async (req, res) => {
-  try {
-    const html = await mailer.renderHtml('auth/successVerification', {
-      appUrl: process.env.APP_URL
-    })
-    return res.status(200).send(html)
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({message: error.message})
-  }
-})
+router.get('/create-initiatives', TestController.createInitiatives)
 
-router.get('/create-initiatives', async (req, res) => {
-  try {
+router.get('/create-challenges', TestController.createChallenges)
 
-    const dimensions = await models.Dimension.findAll()
+router.get('/create-blog-entries', TestController.createBlogEntries)
 
-    // make a loop of 150 iterations
-    for(let i = 0; i < 150; i++) {
-
-
-      const contact = {
-        fullname: faker.person.fullName(),
-        email: faker.internet.email(),
-        phone: faker.phone.number(),
-        keepPrivate: faker.datatype.boolean()
-      }
-      const initiative = {
-        name: faker.lorem.sentence({min: 4, max: 15}),
-        description: faker.lorem.sentence({min: 10, max: 50}),
-        needsAndOffers: faker.lorem.sentence({min: 20, max: 90}),
-        publishedAt: new Date(),
-      }
-      const subdivisions = await models.Subdivision.findAll({include: 'city'})
-      // console.log(JSON.stringify(subdivisions,null,2))
-
-      // get a random subdivision
-      const randomSubdivision = subdivisions[Math.floor(Math.random() * subdivisions.length)]
-
-      const newContact = await models.InitiativeContact.create(contact)
-      const newInitiative = await models.Initiative.create({
-        ...initiative,
-        contactId: newContact.id,
-        subdivisionId: randomSubdivision.id,
-      })
-
-      // pick two random dimensions, different from each other
-      const randomIndex1 = Math.floor(Math.random() * dimensions.length)
-      let randomIndex2 = Math.floor(Math.random() * dimensions.length)
-      while(randomIndex2 === randomIndex1) {
-        randomIndex2 = Math.floor(Math.random() * dimensions.length)
-      }
-      const dimension1 = dimensions[randomIndex1]
-      const dimension2 = dimensions[randomIndex2]
-      
-      // make a 65% chance of adding both dimensions, the rest, add the first one
-      const chance = Math.random()
-      if(chance > 0.35) {
-        await newInitiative.addDimensions([dimension1, dimension2])
-      } else {
-        await newInitiative.addDimensions([dimension1])
-      }
-    }
-
-    return res.status(200).json({
-      message: '150 Initiatives created successfully'
-    })
-
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({message: error.message})
-  }
-})
-
-
-router.get('/create-challenges', async (req, res) => {
-  try {
-    // challenge has:
-    //    dimensionId
-    //    subdivisionId
-    //    needsAndChallenges
-    //    proposal
-    //    inWords
-    
-    const dimensions = await models.Dimension.findAll()
-    const subdivisions = await models.Subdivision.findAll()
-
-    // make a loop of 150 iterations
-    for(let i = 0; i < 150; i++) {
-      const challenge = {
-        needsAndChallenges: faker.lorem.sentence({min: 20, max: 90}),
-        proposal: faker.lorem.sentence({min: 20, max: 90}),
-        inWords: faker.lorem.words({min: 1, max: 2})
-      }
-
-      // get a random subdivision
-      const randomSubdivision = subdivisions[Math.floor(Math.random() * subdivisions.length)]
-      const randomDimension = dimensions[Math.floor(Math.random() * dimensions.length)]
-
-      const newChallenge = await models.Challenge.create({
-        subdivisionId: randomSubdivision.id,
-        dimensionId: randomDimension.id,
-        ...challenge,
-      })
-    }
-
-    return res.status(200).json({
-      message: '150 Challenges created successfully'
-    })
-  } catch {
-    console.error(error)
-    return res.status(500).json({message: error.message})
-  }
-})
-
-
-router.post('/file-test', uploader.single('file'), async (req, res) => {
-  try {
-    console.log(req.file)
-    return res.status(200).json({message: 'File uploaded successfully', file: req.file })
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({message: error.message})
-  }
-})
+router.post('/file-test', 
+  uploader.single('file'),
+  TestController.fileTest
+)
 
 
 module.exports = router;
