ca78811c4b577f699da574bc123c72a82a0fad56
Guillermo Croppi <guillermocroppi@gmail.com>
2025-02-12 17:18:57 -0300

    Add BotResponses model and migration

---
+++ migrations/00016-create-botResponses.js
@@ -0,0 +1,31 @@
+const {Sequelize} = require('sequelize')
+
+module.exports = {
+  async up({context: queryInterface}) {
+    await queryInterface.createTable('BotResponses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER
+      },
+      response: {
+        type: Sequelize.DataTypes.JSON,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+      }
+    });
+  },
+  async down({context: queryInterface}) {
+    await queryInterface.dropTable('BotResponses');
+  }
+};
---
+++ models/botResponse.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+  class BotResponse extends Model {
+
+  }
+
+  BotResponse.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    response: {
+      type: DataTypes.JSON,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'BotResponse',
+    tableName: 'BotResponses',
+    timestamps: true
+  });
+
+  return BotResponse;
+};
