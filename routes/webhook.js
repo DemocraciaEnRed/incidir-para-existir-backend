daa4ffb50a70e0d5c0a4db8212229f07853a1b79
Guillermo Croppi <guillermocroppi@gmail.com>
2025-02-18 19:32:35 -0300

    Add webhook route for handling Twilio messages

---
+++ routes/webhook.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { check, query, body, param } = require('express-validator');
+
+const validate = require('../middlewares/validate');
+const authorize = require('../middlewares/authorize');
+const requiresAnon = require('../middlewares/requiresAnon');
+const BotController = require('../controllers/botController');
+const msg = require('../utils/messages');
+const constants = require('../services/constants');
+const uploader = require('../middlewares/s3');
+
+// initialize router
+const router = express.Router();
+
+// -----------------------------------------------
+// BASE   /webhook
+// -----------------------------------------------
+// POST    /whatsapp
+// -----------------------------------------------
+
+router.post('/whatsapp',
+  BotController.postTwilioWebhook
+)
+
+
+// -----------------------------------------------
